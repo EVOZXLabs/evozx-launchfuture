1ae4f9a091496bad088be66bdde69243e2905715
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-13 10:06:44 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,7 @@
 export const NETWORK = {
+
   chainId: 805,
+
   chainHex: "0x325",
 
   chainName: "EVOZ",
@@ -13,20 +15,39 @@ export const NETWORK = {
   ],
 
   nativeCurrency: {
+
     name: "EVOZ",
+
     symbol: "EVOZ",
+
     decimals: 18
+
   }
+
 };
 
 export const CONTRACTS = {
 
   FACTORY:
-    "0xbA40773bCF0d30e83c4319796Ec45CA31d6e64bB",
+  "0xbA40773bCF0d30e83c4319796Ec45CA31d6e64bB",
 
   EVOZX:
-    "0x032a962F62Fc1cbc15B19767Aa138deA3B454B74",
+  "0x032a962F62Fc1cbc15B19767Aa138deA3B454B74",
 
   TREASURY:
-    "0x50Cd30Ff7f0fbBD9d0FDe1F60DE8c52D6F390c5C"
+  "0x50Cd30Ff7f0fbBD9d0FDe1F60DE8c52D6F390c5C"
+
+};
+
+export const LINKS = {
+
+  EXPLORER:
+  "https://evozscan.com",
+
+  ADDRESS:
+  "https://evozscan.com/address/",
+
+  TX:
+  "https://evozscan.com/tx/"
+
 };
