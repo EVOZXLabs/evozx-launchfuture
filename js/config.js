8bccf759d99c387266e0595903ecfcc8325b796a
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-13 08:34:03 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,39 @@
+// EVOZX LaunchKit
+// Network & Contract Configuration
+
+export const NETWORK = {
+  chainId: 805,
+  chainHex: "0x325",
+
+  chainName: "EVOZ",
+
+  rpcUrls: [
+    "https://rpc.evozscan.com"
+  ],
+
+  blockExplorerUrls: [
+    "https://evozscan.com"
+  ],
+
+  nativeCurrency: {
+    name: "EVOZ",
+    symbol: "EVOZ",
+    decimals: 18
+  }
+};
+
+export const CONTRACTS = {
+  FACTORY:
+    "0xbA40773bCF0d30e83c4319796Ec45CA31d6e64bB",
+
+  EVOZX:
+    "0x032a962F62Fc1cbc15B19767Aa138deA3B454B74",
+
+  TREASURY:
+    "0x50Cd30Ff7f0fbBD9d0FDe1F60DE8c52D6F390c5C"
+};
+
+export const EXPLORER = {
+  ADDRESS: "https://evozscan.com/address/",
+  TX: "https://evozscan.com/tx/"
+};
