036f3a07f92fb5a2e73c30043f4ec67abe05f07f
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-13 10:08:05 +0700

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -1,76 +1,137 @@
-import { BrowserProvider } from "https://esm.sh/ethers@6";
+import {
+  BrowserProvider
+}
+from "https://esm.sh/ethers@6";
 
-import { NETWORK } from "./config.js";
+import {
+  NETWORK
+}
+from "./config.js";
 
 let provider = null;
+
 let signer = null;
-let userAddress = null;
+
+let address = null;
 
 export async function connectWallet() {
+
   if (!window.ethereum) {
-    throw new Error("Web3 wallet not detected.");
+
+    throw new Error(
+      "No Web3 wallet detected."
+    );
+
   }
 
   await window.ethereum.request({
-    method: "eth_requestAccounts"
+
+    method:
+    "eth_requestAccounts"
+
   });
 
-  provider = new BrowserProvider(window.ethereum);
+  provider =
+  new BrowserProvider(
+    window.ethereum
+  );
 
-  signer = await provider.getSigner();
+  signer =
+  await provider.getSigner();
 
-  userAddress = await signer.getAddress();
+  address =
+  await signer.getAddress();
 
   return {
+
     provider,
+
     signer,
-    address: userAddress
+
+    address
+
   };
+
 }
 
 export async function switchToEVOZ() {
-  try {
-    await window.ethereum.request({
-      method: "wallet_switchEthereumChain",
-      params: [
-        {
-          chainId: NETWORK.chainHex
-        }
-      ]
-    });
-  } catch (error) {
-    if (error.code === 4902) {
-      await window.ethereum.request({
-        method: "wallet_addEthereumChain",
-        params: [
-          {
-            chainId: NETWORK.chainHex,
-            chainName: NETWORK.chainName,
 
-            rpcUrls: NETWORK.rpcUrls,
+  try {
+
+    await window.ethereum.request({
+
+      method:
+      "wallet_switchEthereumChain",
+
+      params: [
+
+        {
+          chainId:
+          NETWORK.chainHex
+        }
+
+      ]
+
+    });
+
+  } catch (error) {
+
+    if (error.code === 4902) {
+
+      await window.ethereum.request({
+
+        method:
+        "wallet_addEthereumChain",
+
+        params: [
+
+          {
+
+            chainId:
+            NETWORK.chainHex,
+
+            chainName:
+            NETWORK.chainName,
+
+            rpcUrls:
+            NETWORK.rpcUrls,
 
             blockExplorerUrls:
-              NETWORK.blockExplorerUrls,
+            NETWORK.blockExplorerUrls,
 
             nativeCurrency:
-              NETWORK.nativeCurrency
+            NETWORK.nativeCurrency
+
           }
+
         ]
+
       });
+
     } else {
+
       throw error;
+
     }
+
   }
+
 }
 
-export async function getProvider() {
+export function getProvider() {
+
   return provider;
+
 }
 
-export async function getSigner() {
+export function getSigner() {
+
   return signer;
+
 }
 
-export async function getAddress() {
-  return userAddress;
+export function getAddress() {
+
+  return address;
+
 }
