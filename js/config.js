2fe9de2d0696af5632a65534a74097ea6482f381
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-14 10:24:28 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,7 @@
+// ======================================================
+// EVOZ NETWORK
+// ======================================================
+
 export const NETWORK = {
 
   chainId: 805,
@@ -26,28 +30,126 @@ export const NETWORK = {
 
 };
 
+// ======================================================
+// CONTRACTS
+// ======================================================
+
 export const CONTRACTS = {
 
+  // EVOZX Ultimate Factory
   FACTORY:
-  "0xbA40773bCF0d30e83c4319796Ec45CA31d6e64bB",
+    "0xbA40773bCF0d30e83c4319796Ec45CA31d6e64bB",
 
+  // EVOZX Token
   EVOZX:
-  "0x032a962F62Fc1cbc15B19767Aa138deA3B454B74",
+    "0x032a962F62Fc1cbc15B19767Aa138deA3B454B74",
 
+  // EVOZX Exchange
+  EXCHANGE:
+    "0x24cCb720F7F8b9247FB50A88F6A6a5A5DD7d9ab8",
+
+  // Treasury
   TREASURY:
-  "0x50Cd30Ff7f0fbBD9d0FDe1F60DE8c52D6F390c5C"
+    "0x50Cd30Ff7f0fbBD9d0FDe1F60DE8c52D6F390c5C"
 
 };
+
+// ======================================================
+// LINKS
+// ======================================================
 
 export const LINKS = {
 
   EXPLORER:
-  "https://evozscan.com",
+    "https://evozscan.com",
 
   ADDRESS:
-  "https://evozscan.com/address/",
+    "https://evozscan.com/address/",
 
   TX:
-  "https://evozscan.com/tx/"
+    "https://evozscan.com/tx/"
+
+};
+
+// ======================================================
+// APP INFO
+// ======================================================
+
+export const APP = {
+
+  NAME:
+    "EVOZX LaunchFuture",
+
+  VERSION:
+    "2.0.0"
+
+};
+
+// ======================================================
+// FACTORY FEES
+// (must match Solidity contract)
+// ======================================================
+
+export const FEES = {
+
+  BASE: 10,
+
+  BURNABLE: 5,
+
+  MINTABLE: 20,
+
+  OWNERSHIP: 5,
+
+  MAX_WALLET: 5,
+
+  MAX_TX: 5,
+
+  TRADING_CONTROL: 10,
+
+  BUY_TAX: 20,
+
+  SELL_TAX: 20,
+
+  WEBSITE: 1,
+
+  TELEGRAM: 1,
+
+  TWITTER: 1,
+
+  LOGO: 2
+
+};
+
+// ======================================================
+// TOKEN SETTINGS
+// ======================================================
+
+export const TOKEN_LIMITS = {
+
+  MAX_SUPPLY:
+    1_000_000_000_000,
+
+  MAX_SYMBOL_LENGTH:
+    12,
+
+  MAX_BUY_TAX:
+    10,
+
+  MAX_SELL_TAX:
+    10
+
+};
+
+// ======================================================
+// FACTORY CONSTANTS
+// ======================================================
+
+export const FACTORY_INFO = {
+
+  LAUNCHKIT_VERSION:
+    200,
+
+  EVOZX_DECIMALS:
+    18
 
 };
