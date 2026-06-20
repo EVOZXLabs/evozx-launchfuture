7f2733ba9cb2e2779cd8bdac5206e05319c38fa0
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-20 19:18:34 +0700

    Update upload-logo.js

--- api/upload-logo.js
+++ api/upload-logo.js
@@ -1,54 +1,12 @@
-export const config = {
-    api: {
-        bodyParser: false
-    }
-};
-
 export default async function handler(
     req,
     res
 ) {
 
-    if (req.method !== "POST") {
-
-        return res.status(405).json({
-            success: false,
-            error: "Method not allowed"
-        });
-
-    }
-
-    try {
-
-        const jwt =
-            process.env.PINATA_JWT;
-
-        const pinataRes =
-            await fetch(
-                "https://api.pinata.cloud/data/testAuthentication",
-                {
-                    headers: {
-                        Authorization:
-                            `Bearer ${jwt}`
-                    }
-                }
-            );
-
-        const result =
-            await pinataRes.json();
-
-        return res.status(200).json({
-            success: true,
-            pinata: result
-        });
-
-    } catch (err) {
-
-        return res.status(500).json({
-            success: false,
-            error: err.message
-        });
-
-    }
+    return res.status(200).json({
+        success: true,
+        jwt:
+            !!process.env.PINATA_JWT
+    });
 
 }
